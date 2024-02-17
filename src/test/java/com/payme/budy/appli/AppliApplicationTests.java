945ef46d87d10efecbbf06b40208157d07325851
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-02-17 17:16:40 +0100

    chore: add maven dependencies

---
+++ src/test/java/com/payme/budy/appli/AppliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payme.budy.appli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
